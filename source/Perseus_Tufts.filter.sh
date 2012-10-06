ee288ee6c970efd7ba57590529ecd3a58654db70
Mr. T <taal@camel>
2012-10-06 12:30:50 +0100

    wip...the xml makes my head hurt

--- source/Perseus_Tufts.filter.sh
+++ source/Perseus_Tufts.filter.sh
@@ -11,17 +11,30 @@ parser=$BASE/../parse-dictionary.pl
 
 # De-Buckwalter-ize the xml:
 time pv "$DIR"/Arabic/Lane/opensource/*xml | \
+	perl -pe 's#A\^#>#g' | \
+	perl -pe 's#w\^#&#g' | \
+	perl -pe 's#y\^#}#g' | \
+	perl -pe 's#\^#''#g' | \
+	perl -MEncode::Arabic::Buckwalter -pe \
+		's#<hi rend="ital" TEIform="hi">(.*?)</hi>#<i>$1</i>#gi' | \
+	perl -MEncode::Arabic::Buckwalter -pe \
+		's#<orth orig="" extent="full" lang="ar">(.*?)</orth>#{ encode "utf8", decode "buckwalter", $1; }#gei' | \
 	perl -MEncode::Arabic::Buckwalter -pe \
 		's#<orth orig="" extent="full" lang="ar">(.*?)</orth>#{ encode "utf8", decode "buckwalter", $1; }#gei' | \
 	perl -MEncode::Arabic::Buckwalter -pe \
 		's#<orth extent="full" lang="ar">(.*?)</orth>#{ encode "utf8", decode "buckwalter", $1; }#gei' | \
 	perl -MEncode::Arabic::Buckwalter -pe \
+		's#(<div2 n=")([^"]*?)(" type="root" org="uniform" sample="complete" part="N" TEIform="div2">)#{ print $1; print encode "utf8", decode "buckwalter", $2; print $3; }#gei' | \
+	perl -MEncode::Arabic::Buckwalter -pe \
 		's#(<entryFree id="n\d+" key=")([^"]*?)(" type="main">)#{ print $1; print encode "utf8", decode "buckwalter", $2; print $3; }#gei' | \
 	perl -MEncode::Arabic::Buckwalter -pe \
 		's#<foreign lang="ar" TEIform="foreign">(.*?)</foreign>#{ encode "utf8", decode "buckwalter", $1; }#gei' | \
 	perl -MEncode::Arabic::Buckwalter -pe \
-		's#<orth type="arrow" lang="ar">(.*?)</orth>#{ encode "utf8", decode "buckwalter", $1; }#gei' \
-		> "$DIR"/perseus-tufts-lane-opensource-debuckwalterized.xml
+		's#<orth[^>]*? lang="ar">(.*?)</orth>#{ encode "utf8", decode "buckwalter", $1; }#gei' | \
+		cat > "$DIR"/perseus-tufts-lane-opensource-debuckwalterized.xml
+#	perl -C -pe 's#ا^#أ#gi' | \
+#	perl -C -pe 's#و^#ؤ#gi' | \
+#	perl -C -pe 's#ي^#ئ#gi' | \
 	
 
 #mkdir -p "$OUT_DIR"
